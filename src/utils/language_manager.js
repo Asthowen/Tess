841ea4fb9ad148e184b28cca75ddd19d6f1a9b04
Asthowen <raphael.pilinski@protonmail.com>
2022-07-11 13:50:16 +0200

    Add some doc for LanguageManager

--- src/utils/language_manager.js
+++ src/utils/language_manager.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const fs = require('fs');
 
+
+/**
+ * This class makes it easier to manage languages in Tess.
+ * @class
+ */
 class LanguageManager {
     constructor() {
         this.jsonLanguage = null;
@@ -9,30 +14,57 @@ class LanguageManager {
         this.json = null;
     }
 
+    /**
+     * This method allows to define the folder containing the JSON files of the translations.
+     * @param {String} languageFolder The path to the folder containing the translations.
+     * @return {LanguageManager}
+     */
     setLanguageFolder(languageFolder){
         this.languageFolder = languageFolder;
         return this;
     }
 
+    /**
+     * This method allows to define the language of the JSON used.
+     * @param {String} language The language code, example: fr, en, de...
+     * @return {LanguageManager}
+     */
     setLanguage(language) {
         this.jsonLanguage = language;
         return this;
     }
 
+    /**
+     * This method automatically finds the user's language.
+     * @return {LanguageManager}
+     */
     withLanguageAutoDetection() {
         this.jsonLanguage = Intl.DateTimeFormat().resolvedOptions().locale.replace('-', '_').split('_')[0];
         return this;
     }
 
+    /**
+     * This method allows to read and parse the JSON with the help of the previously defined values and to save it in memory
+     * @return {LanguageManager}
+     */
     build() {
         let filePath = path.join(this.languageFolder, this.jsonLanguage) + '.json';
         if (!fs.existsSync(filePath)) filePath = path.join(this.languageFolder, this.fallbackLanguage) + '.json';
+        if (!fs.existsSync(filePath)) {
+            this.json = {};
+            return this;
+        }
 
         this.json = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
         return this;
     }
 
+    /**
+     * This method allows you to retrieve a translation using its name.
+     * @param {String} key The value to retrieve in the JSON.
+     * @return {String}
+     */
     getTranslation(key) {
         if (!this.json.hasOwnProperty(key)) return '';
         return this.json[key];
